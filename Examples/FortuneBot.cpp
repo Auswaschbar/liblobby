d7ba212dc39c5aa88af340fe8924454da79b313f
Karl-Robert Ernst <k-r.ernst@my-mail.ch>
2009-02-18 22:58:43 +0100

    multichannel support + join #bottest on default

--- Examples/FortuneBot.cpp
+++ Examples/FortuneBot.cpp
@@ -14,7 +14,7 @@ class FortuneBot : public TASServer
 {
 	virtual void LoginSuccess(const std::string& username)
 	{
-		Join("sy");
+		Join("bottest");
 	};
 	
 	virtual void LoginFail(const std::string& reason)
@@ -35,57 +35,54 @@ class FortuneBot : public TASServer
 
 	virtual void Said(const std::string& channame, const std::string& username, const std::string& message)
 	{
-		if (channame == "sy")
+		if (message.find("!fortune") == 0)
 		{
-			if (message.find("!fortune") == 0)
+			const std::string target = "fortune";
+			std::ostringstream convert;
+			FILE *out;
+			out = popen(target.c_str(), "r");
+			char buf[8000];
+			while (fgets(buf, 100, out))
 			{
-				const std::string target = "fortune";
-				std::ostringstream convert;
-				FILE *out;
-				out = popen(target.c_str(), "r");
-				char buf[8000];
-				while (fgets(buf, 100, out))
-				{
-					convert << buf;
-				}
-				pclose(out);
-				std::istringstream stringbuf(convert.str());
-				
-				while (!stringbuf.eof())
-				{
-					std::string line;
-					std::getline(stringbuf,line);
-					SayEx("sy", line);
-				}
+				convert << buf;
 			}
-			else if (message.find("!cookie") == 0)
+			pclose(out);
+			std::istringstream stringbuf(convert.str());
+			
+			while (!stringbuf.eof())
 			{
-				SayEx("sy", " gives a cookie to "+username);
+				std::string line;
+				std::getline(stringbuf,line);
+				SayEx(channame, line);
 			}
-			else if (message.find("!inet") == 0)
+		}
+		else if (message.find("!cookie") == 0)
+		{
+			SayEx(channame, " gives a cookie to "+username);
+		}
+		else if (message.find("!inet") == 0)
+		{
+			std::istringstream stringbuf(message);
+			std::string buf;
+			stringbuf >> buf;
+			stringbuf >> buf;
+			SayEx(channame, ": "+buf+" gains 1 free internets");
+		}
+		else if (username == "Auswaschbar")
+		{
+			// admin commands
+			if (message.find("!quit") == 0)
 			{
-				std::istringstream stringbuf(message);
-				std::string buf;
-				stringbuf >> buf;
-				stringbuf >> buf;
-				SayEx("sy", ": "+buf+" gains 1 free internets");
+				exit(0);
 			}
-			else if (username == "Auswaschbar")
+			else if (message.find("!restart") == 0)
 			{
-				// admin commands
-				if (message.find("!quit") == 0)
-				{
-					exit(0);
-				}
-				else if (message.find("!restart") == 0)
-				{
-					char* args[1];
-					args[0] = "fortunebot";
-					char* env[0];
-					execve ("fortunebot", args, env);
-					SaidPrivate("Auswaschbar", "Fehler");
-					exit(0);
-				}
+				char* args[1];
+				args[0] = "fortunebot";
+				char* env[0];
+				execve ("fortunebot", args, env);
+				SaidPrivate("Auswaschbar", "Fehler");
+				exit(0);
 			}
 		}
 	};
